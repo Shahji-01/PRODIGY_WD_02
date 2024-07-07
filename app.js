98dee0b74d90bc7f817b1703d7cff76e5307a099
Abhinav shah <139350240+Shahji-01@users.noreply.github.com>
2024-07-07 15:48:11 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+let startTime = 0;
+let elapsedTime = 0;
+let timerInterval;
+let isRunning = false;
+
+function timeToString(time) {
+    let diffInHrs = time / 3600000;
+    let hh = Math.floor(diffInHrs);
+
+    let diffInMin = (diffInHrs - hh) * 60;
+    let mm = Math.floor(diffInMin);
+
+    let diffInSec = (diffInMin - mm) * 60;
+    let ss = Math.floor(diffInSec);
+
+    let diffInMs = (diffInSec - ss) * 100;
+    let ms = Math.floor(diffInMs);
+
+    let formattedHH = hh.toString().padStart(2, "0");
+    let formattedMM = mm.toString().padStart(2, "0");
+    let formattedSS = ss.toString().padStart(2, "0");
+    let formattedMS = ms.toString().padStart(2, "0");
+
+    return `${formattedHH}:${formattedMM}:${formattedSS}`;
+}
+
+function print(txt) {
+    document.getElementById("display").innerHTML = txt;
+}
+
+function startStop() {
+    if (!isRunning) {
+        startTime = Date.now() - elapsedTime;
+        timerInterval = setInterval(function printTime() {
+            elapsedTime = Date.now() - startTime;
+            print(timeToString(elapsedTime));
+        }, 10);
+        document.getElementById("startStopBtn").textContent = "Stop";
+        isRunning = true;
+    } else {
+        clearInterval(timerInterval);
+        document.getElementById("startStopBtn").textContent = "Start";
+        isRunning = false;
+    }
+}
+
+function reset() {
+    clearInterval(timerInterval);
+    print("00:00:00");
+    elapsedTime = 0;
+    document.getElementById("startStopBtn").textContent = "Start";
+    isRunning = false;
+    document.getElementById("lapsList").innerHTML = "";
+}
+
+function lap() {
+    if (isRunning) {
+        let lapTime = timeToString(elapsedTime);
+        let lapItem = document.createElement("li");
+        lapItem.textContent = lapTime;
+        document.getElementById("lapsList").appendChild(lapItem);
+    }
+}
